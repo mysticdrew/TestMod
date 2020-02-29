19fa5927212b0e1b037d8c487e1068d322a59a32
mysticdrew <drewhaas@gmail.com>
2020-02-29 14:23:05 -0600

    TestMod for Linkage error

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,44 @@
+package com.example.examplemod;
+
+import journeymap.client.api.ClientPlugin;
+import journeymap.client.api.display.Context;
+import journeymap.client.api.util.UIState;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("examplemod")
+public class ExampleMod
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ExampleMod()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::loadCompleteEvent);
+    }
+
+    private void loadCompleteEvent(FMLLoadCompleteEvent event)
+    {
+        UIState.newInactive(Context.UI.Any, Minecraft.getInstance());
+    }
+
+
+}
